45b6df4a3a45b739f7cf86a73948d47bc47fb480
bjkonglu <konglu@corp.netease.com>
2018-12-06 10:18:06 +0800

    ADD: 数组去重问题

--- src/main/java/com/github/nadal/App.java
+++ src/main/java/com/github/nadal/App.java
@@ -4,10 +4,9 @@ package com.github.nadal;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
