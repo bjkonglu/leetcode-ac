9532827875b7573b3ec897bf20c6a3ad7589ed40
bjkonglu <konglu@corp.netease.com>
2018-12-12 11:33:38 +0800

    ADD: 最大连续子数组问题

---
+++ src/test/java/com/github/nadal/easy/array/TestMaxSubArray.java
@@ -0,0 +1,17 @@
+package com.github.nadal.easy.array;
+
+import org.junit.Test;
+
+/**
+ * Author: lukong
+ * Date: 2018/12/12
+ * Description:
+ */
+public class TestMaxSubArray {
+    @Test
+    public void testSolution() {
+        MaxSubArray maxSubArray = new MaxSubArray();
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        maxSubArray.maxSubArray(nums);
+    }
+}
